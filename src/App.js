e33aa7d0fa44f1d24ab105aa682f0ac6c7e96dd0
MrMishka02 <m.gogia@hotmail.com>
2023-07-26 20:58:12 +0400

    docs: added components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
+import Logo from "./components/Logo/Logo";
 function App() {
   return (
-    <div className="App">
-
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex justify-center w-[20.4375rem] h-[33.938rem]
+       bg-[#15263F] rounded-xl ">
+        <Logo />
+      </div>
     </div>
   );
 }
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import logo from "../../assets/img/equilibrium.jpg"
+
+function Logo() {
+  return (
+    <div className="mt-4 w-[17.375rem] h-[17.375rem]">
+      <img src={logo} className='rounded-xl' alt='Wall img'/>
+    </div>
+  )
+}
+
+export default Logo
