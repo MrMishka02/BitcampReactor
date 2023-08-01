4f4916f0dc2db1e16b10bf0013ec280f56e7e252
MrMishka02 <m.gogia@hotmail.com>
2023-08-01 22:37:44 +0400

    docs: added component and pages folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import Logo from "./components/Logo/Logo";
+import Landing from "./pages/Landing";
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="flex justify-center w-[20.4375rem] h-[33.938rem]
-       bg-[#15263F] rounded-xl ">
-        <Logo />
-      </div>
+    <div>
+      <Landing />
     </div>
   );
 }
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,11 +1,12 @@
-import logo from "../../assets/img/equilibrium.jpg"
+import logo from "../../assets/img/equilibrium.jpg";
 
-function Logo() {
+function Logo(props) {
   return (
     <div className="mt-4 w-[17.375rem] h-[17.375rem]">
-      <img src={logo} className='rounded-xl' alt='Wall img'/>
+      <img src={logo} className="rounded-xl" alt="Wall img" />
+      <h1> { props.name } </h1>
     </div>
-  )
+  );
 }
 
-export default Logo
+export default Logo;
