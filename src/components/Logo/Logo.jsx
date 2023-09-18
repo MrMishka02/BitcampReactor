cf1536e0d993747ba5fd4c38f357211ef1344276
MrMishka02 <m.gogia@hotmail.com>
2023-09-18 23:02:02 +0400

    chore: made some minor changes

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,10 @@
 import logo from "../../assets/img/equilibrium.jpg";
 
-function Logo(props) {
+function Logo({ name }) {
   return (
     <div className="mt-4 w-[17.375rem] h-[17.375rem]">
       <img src={logo} className="rounded-xl" alt="Wall img" />
-      <h1> { props.name } </h1>
+      <h1 className="text-white text-xl uppercase"> {name} </h1>
     </div>
   );
 }
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,14 +1,16 @@
-import Logo from '../components/Logo/Logo'
+import Logo from "../components/Logo/Logo";
 
 function Landing() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="flex justify-center w-[20.4375rem] h-[33.938rem]
-       bg-[#15263F] rounded-xl ">
-        <Logo name='Bitcamp'/>
+      <div
+        className="flex justify-center w-[20.4375rem] h-[33.938rem]
+       bg-[#15263F] rounded-xl "
+      >
+        <Logo name="Bitcamp" />
       </div>
     </div>
-  )
+  );
 }
 
-export default Landing
+export default Landing;
